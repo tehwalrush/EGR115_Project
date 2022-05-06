f6bd1e31366f8f9efe03cc843d5c9701f9ac498f
tehwalrush <97373039+tehwalrush@users.noreply.github.com>
2022-05-05 21:26:37 -0400

    Delete force_calc.py

--- Documents/force_calc.py
+++
@@ -1,6 +0,0 @@
-force = "0"
-while type(force) == str or type(force) == float:
-    mass = input("input mass of the object: ")
-    acceleration = input("input acceleration of the object: ")
-    force = float(mass) * float(acceleration)
-    print("Net force on the object is " + str(force) + " Newtons")
